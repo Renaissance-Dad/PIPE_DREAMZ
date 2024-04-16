1c8013ea83821d00b06c48395d503e06d691e66d
Renaissance Dad <112432132+Renaissance-Dad@users.noreply.github.com>
2024-04-16 19:12:44 +0200

    FIRST MILESTONE
    Simple ROM with moving selector and level one grid.

---
+++ res/resources.h
@@ -0,0 +1,9 @@
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const Image bordertile;
+extern const Image pipesregulartile;
+extern const Image pipesspecialtile;
+extern const SpriteDefinition selectorsprite;
+
+#endif // _RES_RESOURCES_H_
